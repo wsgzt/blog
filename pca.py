b8d767559c48c002b683d26b551c793a340bd26e
wsgzt <wszt.gzt@gmail.com>
2014-04-08 17:41:03 +0800

    Create pca.py

---
+++ pca.py
@@ -0,0 +1,42 @@
+from numpy import *;
+import Image;
+
+def pca(data, dimen) :
+	'''
+	data:
+		an array of sample, normalization processed. 
+	dimen:
+		dimension of the main component.
+	'''
+	# process the data.
+	avgs = [];
+	for d in data :
+		avg = mean(d, axis=0);
+		d -= avg;
+		avgs.append(avg);
+	
+	# covariance matrix.	
+	c = cov(data);
+
+	# get eigenvalues and eigenvectors.
+	values,vectors = linalg.eig(c);
+	
+	# get the max values.
+	index = (argsort(values)[::-1])[0:dimen];
+	values = values[:,index];
+	vectors = vectors[:,index];
+
+	new_data = dot(transpose(vectors), data);
+	restore = dot(vectors, new_data);
+	
+	print '------ old ------'
+	print data;
+	print '------ new ------'
+	print restore;
+
+	return vectors;
+
+if __name__ == '__main__' :
+	pic = Image.open('/home/wszt/图片/vimback1.png');
+	data = array(pic);
+	print data;
